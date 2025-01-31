2cc1d400ddced84eabf298d81654f1ae915e59b5
Alex Borowiak <aborowiak@student.unimelb.edu.au>
2025-01-31 16:54:03 +1100

    Update bar plot bounds to be from start to end of non-zero values

--- src/diagnostic_tools.py
+++ src/diagnostic_tools.py
@@ -24,10 +24,27 @@ def generate_rel_freq_between_year(da, start, end, bins=None):
     return bins, rel_freq
 
 
-def plot_bar(bins, rel_freq , ax=None, **kwargs):
+def plot_bar(bins, rel_freq, ax=None, **kwargs):
+    # Identify indices where rel_freq is not zero
+    non_zero_indices = np.where(rel_freq != 0)[0]
 
-    if ax == None:
+    # Subset the bins and rel_freq to exclude 0-padding
+    start, end = non_zero_indices[0], non_zero_indices[-1] + 1  # Include the rightmost bin
+    bins_subset = bins[start:end + 1]  # +1 to include the right edge bin
+    rel_freq_subset = rel_freq[start:end]
+
+    if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
-    ax.bar(bins[:-1], rel_freq, width=np.diff(bins), align='edge', alpha=0.5, edgecolor='k') #, label=kwargs.get('label', None)
+    ax.bar(bins_subset[:-1], rel_freq_subset, width=np.diff(bins_subset), align='edge', alpha=0.5, edgecolor='k')
+
+    return ax
+    
+# def plot_bar(bins, rel_freq , ax=None, **kwargs):
+
+#     if ax == None:
+#         fig = plt.figure()
+#         ax = fig.add_subplot(111)
+
+#     ax.bar(bins[:-1], rel_freq, width=np.diff(bins), align='edge', alpha=0.5, edgecolor='k') #, label=kwargs.get('label', None)
